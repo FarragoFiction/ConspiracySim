e14b820d484641952cf9ac2a336e4e6a633feda1
jadedResearcher <gtg287y@gmail.com>
2019-07-23 22:18:33 -0400

    simple svg shit, three boxes, you click one it changes color till you mouse up

--- web/scripts/main.dart
+++ web/scripts/main.dart
@@ -2,30 +2,46 @@ import 'dart:html';
 import 'dart:svg';
 Element div = querySelector('#output');
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
-  SvgElement svg = new SvgElement.tag("svg");
+  SvgElement svg = DraggableNode.container;
   div.append(svg);
-  DraggableNode node = new DraggableNode();
-  svg.append(node.rect);
+  svg.append(DraggableNode("Hello").rect);
+  svg.append(DraggableNode("World", x:150).rect);
+  svg.append(DraggableNode("Goodbye", x: 300).rect);
+
 }
 
 class DraggableNode {
   RectElement rect;
-  int width = 100;
-  int height = 100;
+  int width;
+  int height;
+  int x;
+  int y;
+  String text;
+  static SvgElement _container;
+  static SvgElement get container {
+    if(_container == null) {
+      _container = SvgElement.tag("svg"); //removing 'new' bothers me on a fundamental level
+      _container.attributes["width"] = "1000";
+      _container.attributes["height"] = "1000";
+    }
+    return _container;
+  }
 
-  DraggableNode() {
+  DraggableNode(String this.text, {this.width: 100, this.height: 100, this.x: 0, this.y:0}) {
     rect = new RectElement();
     rect.attributes["width"] = "$width";
     rect.attributes["height"] = "$height";
-    rect.attributes["fill"] = "#000000";
+    rect.attributes["x"] = "$x";
+    rect.attributes["y"] = "$y";
+    rect.attributes["fill"] = "#ffffff";
+    rect.attributes["stroke"] = "#000000";
 
     rect.onMouseDown.listen((MouseEvent e) {
       rect.attributes["fill"] = "#ff0000";
     });
 
     rect.onMouseUp.listen((MouseEvent e) {
-      rect.attributes["fill"] = "#000000";
+      rect.attributes["fill"] = "#ffffff";
     });
   }
 }
