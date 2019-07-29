f7ba31f848a43696beb36cddd579e5eee8a9e151
jadedResearcher <gtg287y@gmail.com>
2019-07-28 23:16:27 -0400

    thoughts

--- web/scripts/Graph.dart
+++ web/scripts/Graph.dart
@@ -18,7 +18,41 @@ class Graph {
     }
 
     void distributeNodes() {
-        distributeNodesGrid();
+        distributeNodesCircles();
+    }
+
+    void distributeNodesCircles() {
+        //first, sort nodes by number of edges
+        final List<DraggableNode> nodes = new List<DraggableNode>.from(allNodes.values);
+        print("nodes is $nodes");
+        nodes.sort((a, b) => b.edges.length.compareTo(a.edges.length));
+        startCenterRecurse(nodes);
+        for(final Edge edge in allEdges) {
+            edge.syncToNodes();
+        }
+    }
+
+    void startCenterRecurse(List<DraggableNode> nodes) {
+        final DraggableNode node = nodes.first;
+        node.handleMove(350, 400);
+        nodes.remove(node);
+        handleCenterRecurse(nodes, node, node);
+    }
+
+
+    //if you aren't the origin node, when you place children, make sure to avoid placing things pointed towards the origin
+    void handleCenterRecurse(List<DraggableNode> nodes, DraggableNode centerNode, DraggableNode originNode) {
+        //then, take all the nodes linked to it, and place THEM at regular angles around
+        //the center
+        //make sure to mark them as having been placed.
+        //then, recurse on each of them, placing THEIR children only in the 180 degrees
+
+        //first divide 360 (or 180) degrees by how many edges there are
+        //then, figure out what angle each edge needs to be at
+        //take the angle and arcsin and arccosin to figure out
+        // what x/y to put it at (multiplied by a distance needed to get them far enough from each other)
+        //then, call this method with each node as the center node
+
     }
 
     void distributeNodesGrid() {
